5cf78a20b38d6c4b7b2ed63e20e5e593e4e55205
smk87 <30999133+smk87@users.noreply.github.com>
2019-05-12 14:21:26 +0600

    Front end theme added.

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,77 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use App\Category;
+use App\Tag;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $category1 = Category::create([
+            'name' => 'News'
+        ]);
+
+        $category2 = Category::create([
+            'name' => 'Marketing'
+        ]);
+
+        $category3 = Category::create([
+            'name' => 'Partnership'
+        ]);
+
+        $post1 = Post::create([
+            'title' => 'Dummy title',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus vel fugiat aliquid veniam ratione velit!',
+            'content' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae consequatur quas quos labore quo voluptate?',
+            'category_id' => $category1->id,
+            'image' => 'posts/6.jpg'
+        ]);
+
+        $post2 = Post::create([
+            'title' => 'Lorem, ipsum dolor.',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus vel fugiat aliquid veniam ratione velit!',
+            'content' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae consequatur quas quos labore quo voluptate?',
+            'category_id' => $category2->id,
+            'image' => 'posts/7.jpg'
+        ]);
+
+        $post3 = Post::create([
+            'title' => 'Lorem ipsum dolor sit.',
+            'description' => ' Lorem ipsum dolor sit amet consectetur adipisici ng elit. Temporibus vel fugiat aliquid veniam  ratione velit!',
+            'content' => 'Lorem ipsum, dolor sit amet consectet ur adipisicing elit. Beatae consequatur quas  q uos labore quo v oluptate?',
+            'category_id' => $category3->id,
+            'image' => 'posts/8.jpg'
+        ]);
+
+        $post4 = Post::create([
+            'title' => 'Lorem ipsum dolor sit amet.',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus vel fugiat aliquid veniam ratione velit!',
+            'content' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae consequatur quas quos labore quo voluptate?',
+            'category_id' => $category2->id,
+            'image' => 'posts/9.jpg'
+        ]);
+
+        $tag1 = Tag::create([
+            'name' => 'job'
+        ]);
+
+        $tag2 = Tag::create([
+            'name' => 'customers'
+        ]);
+
+        $tag3 = Tag::create([
+            'name' => 'record'
+        ]);
+
+        $post1->tags()->attach([$tag1->id, $tag2->id]);
+        $post2->tags()->attach([$tag3->id, $tag2->id]);
+        $post3->tags()->attach([$tag1->id, $tag3->id]);
+    }
+}
